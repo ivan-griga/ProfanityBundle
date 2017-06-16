12011b98c889c3e583ae51efef88c2cd9d38ff66
Ivan Griga <griga.rvan@mail.ru>
2017-06-16 11:31:52 +0300

    load profanaties command

---
+++ Command/ProfanitiesLoadCommand.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Vangrg\ProfanityBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Vangrg\ProfanityBundle\Entity\Profanity;
+
+/**
+ * Class ProfanitiesLoadCommand
+ * @package Vangrg\ProfanityBundle\Command
+ */
+class ProfanitiesLoadCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('vangrg:profanities:load')
+            ->setDescription('Load profanities into database.')
+            ->addOption('connection',
+                null,
+                InputOption::VALUE_OPTIONAL,
+                'The connection to use for this command. If empty then use default doctrine connection.'
+            )
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $doctrine = $this->getContainer()->get('doctrine');
+
+        $connectionName = $input->getOption('connection');
+        $em = (empty($connectionName) === true)
+            ? $doctrine->getManagerForClass('Vangrg\ProfanityBundle\Entity\Profanity')
+            : $doctrine->getManager($connectionName);
+
+        $profanities = include(__DIR__ . '/../data/profanities.php');
+
+        $i = 0;
+        foreach ($profanities as $word) {
+            $profanity = new Profanity();
+            $profanity->setWord($word);
+
+            $em->persist($profanity);
+
+            if (($i % 100) === 0) {
+                $em->flush();
+                $em->clear();
+            }
+            ++$i;
+        }
+
+        $em->flush();
+
+        $output->writeln('Success.');
+    }
+}
